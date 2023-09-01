1adb47d51247479bf264aae614a59e30682c04ed
EcossB <erwingcoss21@hotmail.com>
2023-08-31 20:19:30 -0400

    Commenting

--- src/main/java/com/latam/alura/tienda/utils/JPAutils.java
+++ src/main/java/com/latam/alura/tienda/utils/JPAutils.java
@@ -8,6 +8,9 @@ public class JPAutils {
 
     private static EntityManagerFactory em = Persistence.createEntityManagerFactory("tienda");
 
+    /*
+    el entitymanager es quien nos permite hacer el acceso a la base de datos.
+     */
     public static EntityManager getEntityManager(){
         return em.createEntityManager();
     }
